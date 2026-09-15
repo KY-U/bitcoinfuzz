01999895e6463972ff0c887fe42c8cf7a68ea9e8
KY-U <arcocaio10@gmail.com>
2026-09-15 11:29:13 -0300

    refactor(bitcoinkernel): rename hash_bytes_to_hex to bytes_to_hex_reversed Rename inverse byte to hex conversion operation name to communicate intention more explicitly

--- modules/bitcoinkernel/module.cpp
+++ modules/bitcoinkernel/module.cpp
@@ -26,7 +26,7 @@ std::string bytes_to_hex(std::span<const std::byte> bytes) {
   return string_stream.str();
 }
 
-std::string hash_bytes_to_hex(std::span<const std::byte> bytes) {
+std::string bytes_to_hex_reversed(std::span<const std::byte> bytes) {
   std::stringstream string_stream;
   string_stream << std::hex;
   for (const auto byte : bytes | std::views::reverse) {
@@ -78,7 +78,7 @@ char *libbitcoinkernel_transaction(std::span<const uint8_t> buffer) {
 
     const auto txid_bytes = transaction.Txid().ToBytes();
     std::string result = "txid=";
-    result.append(hash_bytes_to_hex(txid_bytes));
+    result.append(bytes_to_hex_reversed(txid_bytes));
     result.append(";");
 
     const auto txins = transaction.Inputs();
@@ -89,7 +89,7 @@ char *libbitcoinkernel_transaction(std::span<const uint8_t> buffer) {
       result.append("index=");
       result.append(std::to_string(outpoint_index));
       result.append("txid=");
-      result.append(hash_bytes_to_hex(outpoint_txid_bytes));
+      result.append(bytes_to_hex_reversed(outpoint_txid_bytes));
       result.append(";");
     }
 
@@ -115,13 +115,13 @@ char *libbitcoinkernel_block(std::span<const uint8_t> buffer) {
     btck::Block block{raw_span};
 
     const auto block_hash_bytes = block.GetHash().ToBytes();
-    std::string result = hash_bytes_to_hex(block_hash_bytes);
+    std::string result = bytes_to_hex_reversed(block_hash_bytes);
 
     const auto txs = block.Transactions();
     for (const auto &tx : txs) {
       const auto txid_bytes = tx.Txid().ToBytes();
       result.append("txid=");
-      result.append(hash_bytes_to_hex(txid_bytes));
+      result.append(bytes_to_hex_reversed(txid_bytes));
       result.push_back(';');
     }
     return strdup(result.c_str());
@@ -162,7 +162,7 @@ char *libbitcoinkernel_block_check(std::span<const uint8_t> buffer) {
         std::to_string(static_cast<int>(state.GetBlockValidationResult())));
     result.append(";hash=");
     const auto block_hash_bytes = block.GetHash().ToBytes();
-    result.append(hash_bytes_to_hex(block_hash_bytes));
+    result.append(bytes_to_hex_reversed(block_hash_bytes));
     result.append(";txs=");
     result.append(std::to_string(block.CountTransactions()));
     result.push_back(';');
@@ -211,7 +211,7 @@ char *libbitcoinkernel_transaction_eval(std::span<const uint8_t> buffer) {
     }
 
     std::string result =
-        hash_bytes_to_hex(std::as_bytes(std::span{witness_hash}));
+        bytes_to_hex_reversed(std::as_bytes(std::span{witness_hash}));
     result += std::to_string(transaction_bytes.size());
     return strdup(result.c_str());
   } catch (...) {
